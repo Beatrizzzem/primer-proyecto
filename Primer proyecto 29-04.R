1c168bf3a34dfefe98825286cabd17048325360d
Beatrizzzem <bespinoza@utem.cl>
2022-04-29 21:05:36 -0400

    Creo mi Primer proyecto 29-04.R

---
+++ Primer proyecto 29-04.R
@@ -0,0 +1,27 @@
+# Suma
+4 + 4 
+
+# Multiplicación
+3 * 5
+
+# División
+4 / 6
+
+#Elevado
+2 ^ 2
+3 ** 2
+
+# Raíz 
+4**1/2
+
+# se guarda en respuesta la operación 
+# 4 + 4
+respuesta <-4 + 4
+
+# Muestra la información que uno decide 
+print(respuesta)
+
+
+# Almacenando texto
+respuesta <- "Que tengn un bonito descanso"
+print(respuesta)
